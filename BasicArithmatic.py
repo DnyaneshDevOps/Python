263e2fecc49de505c1e67e974b76f1580e149b60
dnyanesh-ai <dnyaneshwar.rajmane@visnet.in>
2025-03-07 11:18:33 +0530

    Basic Arithmatic

---
+++ BasicArithmatic.py
@@ -0,0 +1,8 @@
+a = 10
+b = 5
+sum_result = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+print(f"Sum: {sum_result}, Difference: {difference}, Product: {product}, Quotient: {quotient}")
